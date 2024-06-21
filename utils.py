1044e81585eb4d8364333e5dbe53d48fbab51e49
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-06-21 18:11:56 +0100

    Updated to fix formatting and data upload

--- utils.py
+++ utils.py
@@ -1,15 +1,19 @@
 from arcgis.gis import GIS
 from conn import get_redis
-from data_definitions import AGOLGeoJSONUploadPayload, ExportToArcGISRequestPayload, GeodesignhubFeatureProperties
-from geojson import Point, Polygon, LineString, Feature, FeatureCollection
-from typing import List
+from data_definitions import (
+    ExportToArcGISRequestPayload,    
+    AGOLItemDetails,
+)
+import geojson
+from geojson import FeatureCollection
+import json
 from dataclasses import asdict
-from os import environ
-from dacite import from_dict
-from data_definitions import AGOLGeoJSONUploadPayload
+from data_definitions import AGOLItemDetails
 import logging
+
 logger = logging.getLogger("esri-gdh-bridge")
 from dotenv import load_dotenv, find_dotenv
+
 load_dotenv(find_dotenv())
 ENV_FILE = find_dotenv()
 
@@ -17,35 +21,38 @@ if ENV_FILE:
     load_dotenv(ENV_FILE)
 r = get_redis()
 
-class ArcGISHelper():
+
+class ArcGISHelper:
     def __init__(self, agol_token: str):
         self.agol_token = agol_token
         # self.client_id = client_id
 
-    def create_gis_object(self)->GIS:
-        gis = GIS("https://igcollab.maps.arcgis.com/",token = self.agol_token)
+    def create_gis_object(self) -> GIS:
+        gis = GIS("https://www.arcgis.com/", token=self.agol_token)
         return gis
 
+
 def export_design_json_to_agol(submit_to_arcgis_request: ExportToArcGISRequestPayload):
-    
+
     _gdh_design_details = submit_to_arcgis_request.gdh_design_details
     agol_token = submit_to_arcgis_request.agol_token
-    # my_arcgis_helper = ArcGISHelper(agol_token= agol_token)
-    # gis = my_arcgis_helper.create_gis_object()
+    my_arcgis_helper = ArcGISHelper(agol_token=agol_token)
+    gis = my_arcgis_helper.create_gis_object()
 
-    _gdh_design_feature_collection:FeatureCollection = _gdh_design_details.design_geojson.geojson
-    # point_feature_list: List[Feature] = []
-    # linestring_feature_list: List[Feature] = []
-    # polygon_feature_list: List[Feature] = []
+    _gdh_design_feature_collection: FeatureCollection = (
+        _gdh_design_details.design_geojson.geojson
+    )
+    agol_item_details = AGOLItemDetails(
+        title=_gdh_design_details.design_name,
+        snippet=_gdh_design_details.project_id,
+        description=_gdh_design_details.design_id,
+        type="GeoJson",
+    )
 
-    for feature in _gdh_design_feature_collection['features']:
-        _feature_properties = from_dict(data_class = GeodesignhubFeatureProperties, data = feature['properties'])
-        _feature_properties.design_title = _gdh_design_details.design_name       
-        
-        feature['properties'] = {}
-        print(_feature_properties)
-        print(feature)
-        
-        # feature_item = gis.content.add(json.loads(json.dumps(asdict(_feature_properties))), feature)
-        # feature_item.publish()
-        
+    geojson_item = gis.content.add(
+        json.loads(json.dumps(asdict(agol_item_details))), json.loads(geojson.dumps(_gdh_design_feature_collection))
+    )
+    feature_layer_item = geojson_item.publish()
+    # TODO: Display the FL title etc. from the ArcGIS link
+    # Show me the item
+    # Show me the map
