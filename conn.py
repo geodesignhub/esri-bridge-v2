e3b1dcf3f1cb0879816e946720235bcf8083bc1c
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-12-17 13:26:47 +0000

    Updated connection

--- conn.py
+++ conn.py
@@ -1,10 +1,11 @@
 from os import environ as env
 from redis import ConnectionPool, Redis
-
+from urllib.parse import urlparse
 
 def get_redis():
     # A method to get the redis instance and is used globally
     redis_url = env.get("REDIS_URL", "redis://local.test:6379")
     connection_pool = ConnectionPool.from_url(redis_url)
-    redis = Redis(connection_pool=connection_pool, ssl_cert_reqs=None)
+    url = urlparse(env.get("REDIS_URL"))
+    redis = Redis(connection_pool=connection_pool, ssl=(url.scheme == "rediss"), ssl_cert_reqs=None)
     return redis
