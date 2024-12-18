13cdc191eb5271cdee5150eb1db60d9c79f36bdb
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2024-12-18 09:46:29 +0000

    Update conn.py

--- conn.py
+++ conn.py
@@ -3,6 +3,6 @@ from urllib.parse import urlparse
 import redis
 def get_redis():
 
-    url = urlparse(os.environ.get("REDIS_URL"))
+    url = urlparse(os.environ.get("REDIS_URL", "redis://localhost:6379"))
     r = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=(url.scheme == "rediss"), ssl_cert_reqs=None)
     return r 
